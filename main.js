0ed7d5a0371635d9948f8c2de6806e0c622f7cf1
2joopiter <veryberrylike3@gmail.com>
2023-11-10 12:58:01 +0900

    복수개 요소가 반복되면서 이벤트 연결(중요)

--- main.js
+++ main.js
@@ -1,10 +1,60 @@
-const section = document.querySelector('section');
-const articles = section.querySelectorAll('article'); // QuerySelectorAll로 찾은 배열은 유사배열
-const btn = section.querySelector('.btn');
-const childrenEl = section.children; // 마찬가지로 유사배열. 모든 자식요소를 볼 수 있음
-const parentEl = btn.parentElement; // 직계부모는 하나이므로 단수로 나옴. parentElement.parentElement 이렇게 두번 써주면 직계부모 하나 위의 부모가 나옴
-const grandParentEl = btn.closest('body'); // 한번에 조상까지 찾아줌
-const prevEl = btn.previousElementSibling;
-const nextEl = btn.nextElementSibling;
-const siblings = btn.parentElement.children;
-console.log(siblings);
+const btns = document.querySelectorAll('.list1 li');
+console.log(btns);
+
+/*
+  DOM (Document Object Model) : wed Api
+  : 브라우저가 HTML 태그를 해석해서 렌더링을 할 때 자바스크립트로 하여금 생성된 요소를 제어할 수 있도록 객체형태로 만들어주는 API (Application Programing Interface; 프로그래밍 인터페이스)
+*/
+console.dir(btns[0]); // 객체형태로 구조를 볼 수 있음
+
+/*
+btns[0].onclick = () => {
+	console.log('button1');
+};
+
+이 양식은 덮어쓰기가 가능해서 권장되지 않음
+*/
+
+/*
+btns[0].addEventListener('click', () => {
+	console.log('엄청 중요한 작업');
+});
+
+btns[0].addEventListener('click', () => {
+	console.log('실수로 연결한 쓸데없는 작업');
+});
+
+*/
+
+//가급적이면 addEventListener를 이용
+
+//MISSION. 3개의 btns 요소를 forEach로 반복을 돌면서 각 버튼 클릭시 console.log(button1~3)까지 각각 찍히도록 만들기.
+
+/*
+btns.forEach((txt, index) => {
+	console.log(btns[index]);
+});
+
+function msg(btns)
+*/
+
+/*
+btns[0].addEventListener('click', () => {
+	console.log('button1');
+});
+
+btns[1].addEventListener('click', () => {
+	console.log('button2');
+});
+
+btns[2].addEventListener('click', () => {
+	console.log('button3');
+});
+
+*/
+
+btns.forEach((btn, index) => {
+	btn.addEventListener('click', () => {
+		console.log(`button${index + 1}`);
+	});
+});
